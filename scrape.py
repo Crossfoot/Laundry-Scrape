c97ad5ebe087d22b3f84fec992a517e6b21bb24c
Samuel Gross <samuelgross@Samuels-MacBook-Pro.local>
2019-11-27 22:13:46 -0700

    turned 4 if statements into a for loop for better reading

--- scrape.py
+++ scrape.py
@@ -24,19 +24,8 @@ def scrape_site(): #scrapes site for availability of washers, and
   site = requests.get('http://washalert.washlaundry.com/washalertweb/calpoly/WASHALERtweb.aspx?location=950de8d6-9345-4a4f-9f64-f3a1daa6b865')
   site_data = bs4.BeautifulSoup(site.text, 'html.parser') 
   data = [time.strftime('%a %d %b %Y, %I:%M%p', time.localtime())]
-  if site_data.find_all('tr')[7].find_all('td')[2].string == 'Available':
-    data.append('1')
-  else:
-    data.append('0')
-  if site_data.find_all('tr')[8].find_all('td')[2].string == 'Available':
-    data.append('1')
-  else:
-    data.append('0')
-  if site_data.find_all('tr')[9].find_all('td')[2].string == 'Available':
-    data.append('1')
-  else:
-    data.append('0')
-  if site_data.find_all('tr')[10].find_all('td')[2].string == 'Available':
+  for i in range(7, 11):
+    if site_data.find_all('tr')[i].find_all('td')[2].string == 'Available':
     data.append('1')
   else:
     data.append('0')
